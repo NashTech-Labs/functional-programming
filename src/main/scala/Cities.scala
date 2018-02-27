b16259ca8237cd0264fb462c4a70424f4b33c53d
Mahesh Chand <mahesh.kndpl@gmail.com>
2018-02-27 11:26:09 +0530

    added find city example using imperiative style

---
+++ src/main/scala/Cities.scala
@@ -0,0 +1,22 @@
+import scala.util.control.Breaks._
+
+object Cities extends App{
+
+  //imperiative style of coding
+  def findCity(list: List[String], city: String) = {
+    var flag = false
+
+    breakable {
+      for(l <- list){
+        if(l == "Delhi") {
+          flag = true
+          break
+        }
+      }
+    }
+  flag
+  }
+
+  val list = List("Delhi", "Haryana", "Mumbai", "Bangalore", "Hyderabad", "Pune")
+  println("is City found ? " + findCity(list, "Delhi"))
+}
