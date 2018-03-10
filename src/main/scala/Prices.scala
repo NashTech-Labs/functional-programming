5fe9204b09f856f85e0f636dad942ff21bcc9d9b
Mahesh Chand <mahesh.kndpl@gmail.com>
2018-03-10 20:34:33 +0530

    calculated total prices using imperative style

---
+++ src/main/scala/Prices.scala
@@ -0,0 +1,19 @@
+
+/*
+ find the total of prices greater than $20, discounted by 10%.
+ */
+object Prices extends App{
+
+  val prices = List(10,12,15,25,20,18,30,21,48)
+
+  var totalDiscountPrices = 0.0
+
+  for(p <- prices){
+    if(p > 20){
+      totalDiscountPrices = totalDiscountPrices + p * 0.9
+    }
+  }
+
+  println(s"total of discounted prices $totalDiscountPrices")
+
+}
